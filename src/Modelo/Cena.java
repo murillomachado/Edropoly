38ec54a86909f80e0aed0232d24925254954f0f2
murillomachado <murillo9967@hotmail.com>
2016-08-30 01:50:10 -0300

    Revert "Views"
    This reverts commit 4032b547b55afd0b3e31fd4befbb66e327536f7e.

--- src/Modelo/Cena.java
+++ src/Modelo/Cena.java
@@ -5,13 +5,24 @@
  */
 package Modelo;
 
+import java.awt.FileDialog;
+import java.awt.Frame;
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.nio.ByteBuffer;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
+import java.util.Map;
+import java.util.StringTokenizer;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
